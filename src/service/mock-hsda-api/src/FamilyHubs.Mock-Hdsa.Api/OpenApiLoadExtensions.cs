c140352c3566580a6da2ae09484b12d3ea0e4add
Phil Davies <github@phildavies.co.uk>
2024-07-23 13:54:28 +0100

    (try) reading the spec from the web - doesn't work as the spec uses v3.1.0 and Microsoft.OpenApi.Readers only supports openapi 3.0.3. the differences in the spec are...
    JSON Schema 2020-12 alignment
    Webhooks object at the root level
    PathItem object can be empty
    New field for Path Templates
    Relaxed naming restrictions for components
    
    so we'll download the spec, try and downgrade it to 3.0.3 and try loading it from disk instead

---
+++ src/service/mock-hsda-api/src/FamilyHubs.Mock-Hdsa.Api/OpenApiLoadExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.OpenApi.Readers;
+
+namespace FamilyHubs.Mock_Hdsa.Api;
+
+public static class OpenApiLoadExtensions
+{
+    public static async Task AddOpenApiSpec(this IServiceCollection services, string requestUri)
+    {
+        //todo: get from file instead
+        // Load the OpenAPI document
+        using var httpClient = new HttpClient();
+        var openApiJson = await httpClient.GetStringAsync(requestUri);
+        var reader = new OpenApiStringReader();
+        var openApiDoc = reader.Read(openApiJson, out var diagnostic);
+
+        services.AddSingleton(openApiDoc);
+    }
+}
