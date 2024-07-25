31e0a02f9d7b4217b19b8a867288301e596e9920
Phil Davies <github@phildavies.co.uk>
2024-07-25 14:10:27 +0100

    wip

--- src/service/mock-hsda-api/src/FamilyHubs.Mock-Hdsa.Api/MockResponseGenerator.cs
+++ src/service/mock-hsda-api/src/FamilyHubs.Mock-Hdsa.Api/MockResponseGenerator.cs
@@ -9,6 +9,11 @@ public class MockResponseGenerator()
         //todo: load response from db according to operation and customHeaderValue
         //todo: return data according to request params, such as embedding flags, page info, format etc.
 
+        if (operation.OperationId == "getAPIMetaInformation")
+        {
+            return (200, "{\r\n  \"version\": \"3.0\",\r\n  \"profile\": \"https://todo/put/our/profile/uri/here\",\r\n  \"openapi_url\": \"https://raw.githubusercontent.com/openreferral/specification/3.0/schema/openapi.json\"\r\n}\r\n");
+        }
+
         // Generate a mock response based on the operation and custom header value
         // For simplicity, we'll just return a 200 OK response with a simple JSON payload
         return (200, "{\"message\": \"Hello, world!\"}");
