a6bb633769ce6470c4b6dc4b492cd1eb981a2edf
Phil Davies <github@phildavies.co.uk>
2024-07-25 13:46:31 +0100

    wip

---
+++ src/service/mock-hsda-api/src/FamilyHubs.Mock-Hdsa.Api/MockResponseGenerator.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace FamilyHubs.Mock_Hdsa.Api;
+
+public class MockResponseGenerator()
+{
+    public (int statusCode, string responseBody) GenerateResponse(OpenApiOperation operation, string customHeaderValue)
+    {
+        //todo: load response from db according to operation and customHeaderValue
+        //todo: return data according to request params, such as embedding flags, page info, format etc.
+
+        // Generate a mock response based on the operation and custom header value
+        // For simplicity, we'll just return a 200 OK response with a simple JSON payload
+        return (200, "{\"message\": \"Hello, world!\"}");
+    }
+}
--- src/service/mock-hsda-api/src/FamilyHubs.Mock-Hdsa.Api/RequestHandler.cs
+++ src/service/mock-hsda-api/src/FamilyHubs.Mock-Hdsa.Api/RequestHandler.cs
@@ -2,16 +2,6 @@
 
 namespace FamilyHubs.Mock_Hdsa.Api;
 
-public class MockResponseGenerator()
-{
-    public (int statusCode, string responseBody) GenerateResponse(OpenApiOperation operation, string customHeaderValue)
-    {
-        // Generate a mock response based on the operation and custom header value
-        // For simplicity, we'll just return a 200 OK response with a simple JSON payload
-        return (200, "{\"message\": \"Hello, world!\"}");
-    }
-}
-
 public static class RequestHandler
 {
     public static async Task Handle(HttpContext context)
@@ -32,7 +22,7 @@ public static class RequestHandler
             return;
         }
 
-        var customHeaderValue = context.Request.Headers["X-Custom-Header"].FirstOrDefault();
+        var customHeaderValue = context.Request.Headers["X-Mock-Response-Id"].FirstOrDefault();
         var (statusCode, responseBody) = mockGenerator.GenerateResponse(operation, customHeaderValue);
 
         context.Response.StatusCode = statusCode;
