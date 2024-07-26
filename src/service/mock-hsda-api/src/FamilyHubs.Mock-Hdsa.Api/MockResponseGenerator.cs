215d8f1d587c9b4c1dc2c0f154a27c4842e0784f
Phil Davies <github@phildavies.co.uk>
2024-07-26 12:48:10 +0100

    wip

--- src/service/mock-hsda-api/src/FamilyHubs.Mock-Hdsa.Api/MockResponseGenerator.cs
+++ src/service/mock-hsda-api/src/FamilyHubs.Mock-Hdsa.Api/MockResponseGenerator.cs
@@ -4,18 +4,21 @@ namespace FamilyHubs.Mock_Hdsa.Api;
 
 public class MockResponseGenerator()
 {
-    public (int statusCode, string responseBody) GenerateResponse(OpenApiOperation operation, string customHeaderValue)
+    //public (int statusCode, string responseBody) GenerateResponse(OpenApiOperation operation, string customHeaderValue)
+    public Task<(int statusCode, string responseBody)> GetMockResponseAsync(
+        string operationName, string? scenarioName, string pathTemplate, string? pathParams, string? queryParams)
     {
         //todo: load response from db according to operation and customHeaderValue
         //todo: return data according to request params, such as embedding flags, page info, format etc.
 
-        if (operation.OperationId == "getAPIMetaInformation")
+        //if (operation.OperationId == "getAPIMetaInformation")
+        if (operationName == "getAPIMetaInformation")
         {
-            return (200, "{\r\n  \"version\": \"3.0\",\r\n  \"profile\": \"https://todo/put/our/profile/uri/here\",\r\n  \"openapi_url\": \"https://raw.githubusercontent.com/openreferral/specification/3.0/schema/openapi.json\"\r\n}\r\n");
+            return Task.FromResult((200, "{\r\n  \"version\": \"3.0\",\r\n  \"profile\": \"https://todo/put/our/profile/uri/here\",\r\n  \"openapi_url\": \"https://raw.githubusercontent.com/openreferral/specification/3.0/schema/openapi.json\"\r\n}\r\n"));
         }
 
         // Generate a mock response based on the operation and custom header value
         // For simplicity, we'll just return a 200 OK response with a simple JSON payload
-        return (200, "{\"message\": \"Hello, world!\"}");
+        return Task.FromResult((200, "{\"message\": \"Hello, world!\"}"));
     }
 }
--- src/service/mock-hsda-api/src/FamilyHubs.Mock-Hdsa.Api/RequestHandler.cs
+++ src/service/mock-hsda-api/src/FamilyHubs.Mock-Hdsa.Api/RequestHandler.cs
@@ -4,6 +4,40 @@ namespace FamilyHubs.Mock_Hdsa.Api;
 
 public static class RequestHandler
 {
+    //public static async Task Handle(HttpContext context)
+    //{
+    //    var openApiDoc = context.RequestServices.GetRequiredService<OpenApiDocument>();
+    //    var mockGenerator = context.RequestServices.GetRequiredService<MockResponseGenerator>();
+
+    //    var path = context.Request.Path.Value;
+    //    var method = context.Request.Method;
+
+    //    //todo: how best to handle paths with params? we don't want to reinvent asp.net's routing
+    //    //todo: handle query params
+    //    // for individual items, we can use the id as the lookup for the response
+    //    // for lists, do we use the header scenario name to retrieve an array of json and hand code the paging (using the incoming paging params)
+    //    // for embedding options, do we combine the scenario name with the embedding param?
+    //    // do we have ordered params for sub responses according to the scenario, or have custom tables for different operations/responses?
+    //    // or one table with nullable columns or key-value params etc. and use the params applicable for the operation - could then make it generic
+
+    //    var pathItem = openApiDoc.Paths.FirstOrDefault(p => p.Key == path).Value;
+    //    var operation = pathItem?.Operations.FirstOrDefault(o => o.Key.ToString().Equals(method, StringComparison.OrdinalIgnoreCase)).Value;
+
+    //    if (operation == null)
+    //    {
+    //        context.Response.StatusCode = StatusCodes.Status404NotFound;
+    //        await context.Response.WriteAsync("Endpoint not found in OpenAPI spec");
+    //        return;
+    //    }
+
+    //    var customHeaderValue = context.Request.Headers["X-Mock-Response-Id"].FirstOrDefault();
+    //    var (statusCode, responseBody) = mockGenerator.GenerateResponse(operation, customHeaderValue);
+
+    //    context.Response.StatusCode = statusCode;
+    //    context.Response.ContentType = "application/json";
+    //    await context.Response.WriteAsync(responseBody);
+    //}
+
     public static async Task Handle(HttpContext context)
     {
         var openApiDoc = context.RequestServices.GetRequiredService<OpenApiDocument>();
@@ -12,28 +46,49 @@ public static class RequestHandler
         var path = context.Request.Path.Value;
         var method = context.Request.Method;
 
-        //todo: how best to handle paths with params? we don't want to reinvent asp.net's routing
-        //todo: handle query params
-        // for individual items, we can use the id as the lookup for the response
-        // for lists, do we use the header scenario name to retrieve an array of json and hand code the paging (using the incoming paging params)
-        // for embedding options, do we combine the scenario name with the embedding param?
-        // do we have ordered params for sub responses according to the scenario, or have custom tables for different operations/responses?
-        // or one table with nullable columns or key-value params etc. and use the params applicable for the operation - could then make it generic
-
-        var pathItem = openApiDoc.Paths.FirstOrDefault(p => p.Key == path).Value;
-        var operation = pathItem?.Operations.FirstOrDefault(o => o.Key.ToString().Equals(method, StringComparison.OrdinalIgnoreCase)).Value;
-
-        if (operation == null)
+        var (pathTemplate, pathParameters) = FindMatchingPathTemplate(openApiDoc, path);
+        if (pathTemplate == null)
         {
             context.Response.StatusCode = StatusCodes.Status404NotFound;
             await context.Response.WriteAsync("Endpoint not found in OpenAPI spec");
             return;
         }
 
-        var customHeaderValue = context.Request.Headers["X-Mock-Response-Id"].FirstOrDefault();
-        var (statusCode, responseBody) = mockGenerator.GenerateResponse(operation, customHeaderValue);
+        var operation = openApiDoc.Paths[pathTemplate].Operations
+            .FirstOrDefault(o => o.Key.ToString().Equals(method, StringComparison.OrdinalIgnoreCase)).Value;
 
-        context.Response.StatusCode = statusCode;
+        if (operation == null)
+        {
+            context.Response.StatusCode = StatusCodes.Status405MethodNotAllowed;
+            await context.Response.WriteAsync("Method not allowed for this endpoint");
+            return;
+        }
+
+        var operationName = operation.OperationId ?? $"{method}_{pathTemplate.Replace("/", "_")}";
+        string? scenarioName = context.Request.Headers["X-Mock-Response-Id"].FirstOrDefault();
+        var queryParams = string.Join("&", context.Request.Query.OrderBy(q => q.Key).Select(q => $"{q.Key}={q.Value}"));
+
+        //todo: path params
+
+        //var (statusCode, responseBody) = mockGenerator.GenerateResponse(operation, customHeaderValue);
+        var (statusCode, responseBody) = await mockGenerator.GetMockResponseAsync(operationName, scenarioName, pathTemplate, "todo:path params", queryParams);
+
+        //todo:
+        //if (response == null)
+        //{
+        //    context.Response.StatusCode = StatusCodes.Status404NotFound;
+        //    await context.Response.WriteAsync("No matching mock response found");
+        //    return;
+        //}
+
+        // Replace path parameters in the response
+        // not needed?
+        //foreach (var param in pathParameters)
+        //{
+        //    response = response.Replace($"{{{param.Key}}}", param.Value);
+        //}
+
+        context.Response.StatusCode = StatusCodes.Status200OK;
         context.Response.ContentType = "application/json";
         await context.Response.WriteAsync(responseBody);
     }
