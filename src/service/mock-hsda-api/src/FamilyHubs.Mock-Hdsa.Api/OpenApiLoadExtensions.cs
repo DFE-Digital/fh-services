d9e9b5471002ef848a85c7fefb6ad407f41d5181
Zac-AND-Digital <zac.king@and.digital>
2024-08-12 14:50:36 +0100

    refactor: remove duplicate database seeding

--- src/service/mock-hsda-api/src/FamilyHubs.Mock-Hdsa.Api/OpenApiLoadExtensions.cs
+++ src/service/mock-hsda-api/src/FamilyHubs.Mock-Hdsa.Api/OpenApiLoadExtensions.cs
@@ -8,7 +8,7 @@ public static class OpenApiLoadExtensions
     public static OpenApiDocument AddOpenApiSpecFromFile(this IServiceCollection services)
     {
         // Load the OpenAPI document from a local file
-        var openApiPath = Path.Combine(Directory.GetCurrentDirectory(), @"..\..\spec\schema\openapi.json");
+        var openApiPath = Path.Combine(Directory.GetCurrentDirectory(), @"../../spec/schema/openapi.json");
         using var stream = File.OpenRead(openApiPath);
         var reader = new OpenApiStreamReader();
         var openApiDoc = reader.Read(stream, out var diagnostic);
