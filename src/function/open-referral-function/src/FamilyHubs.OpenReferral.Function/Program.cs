9db7340b767be6104c89c6c86ec7d71334d5f657
Zac-AND-Digital <zac.king@and.digital>
2024-09-16 16:31:33 +0100

    chore: remove todo

--- src/function/open-referral-function/src/FamilyHubs.OpenReferral.Function/Program.cs
+++ src/function/open-referral-function/src/FamilyHubs.OpenReferral.Function/Program.cs
@@ -6,7 +6,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-// TODO: 2. Unit Test Update
 // TODO: 3. Fix: Plurals for DEDS Db Table Names (Shared Kernel 2.8.1)
 
 IHost host = new HostBuilder()
