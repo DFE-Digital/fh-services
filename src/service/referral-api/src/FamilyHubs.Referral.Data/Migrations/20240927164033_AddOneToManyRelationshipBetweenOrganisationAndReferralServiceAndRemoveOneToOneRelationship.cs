c926efcdc1009c551fb6d441ce88f0a34db6a690
Zac-AND-Digital <zac.king@and.digital>
2024-10-01 13:34:20 +0100

    refactor: modify migration to add foreign key values

--- src/service/referral-api/src/FamilyHubs.Referral.Data/Migrations/20240927164033_AddOneToManyRelationshipBetweenOrganisationAndReferralServiceAndRemoveOneToOneRelationship.cs
+++ src/service/referral-api/src/FamilyHubs.Referral.Data/Migrations/20240927164033_AddOneToManyRelationshipBetweenOrganisationAndReferralServiceAndRemoveOneToOneRelationship.cs
@@ -10,6 +10,45 @@ namespace FamilyHubs.Referral.Data.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.AddColumn<long>(
+                name: "OrganizationId",
+                table: "ReferralServices",
+                type: "bigint",
+                nullable: true);
+
+            /*
+             * We need to manually update the new OrganizationId column of each existing ReferralService with their
+             * Organisation, where the ReferralServiceId matches the ReferralService's ID before that column is deleted
+             * from the Organisations table.
+             *
+             * If there are any ReferralServices with a NULL OrganizationId after this step, we just delete them because
+             * this means they are just dodgy data and would cause errors in the main application anyway (even before this migration),
+             * as even the old code had a required relationship between ReferralServices and Organisations.
+             *
+             * Afterwards we can add the foreign key with intact data and then alter OrganizationId to make it NOT NULL
+             */
+            migrationBuilder.Sql("UPDATE [dbo].[ReferralServices] SET [OrganizationId] = [o].[Id] FROM [dbo].[Organisations] AS [o] WHERE [dbo].[ReferralServices].[Id] = [o].[ReferralServiceId]");
+            migrationBuilder.Sql("DELETE FROM [dbo].[ReferralServices] WHERE [OrganizationId] IS NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ReferralServices_Organisations_OrganizationId",
+                table: "ReferralServices",
+                column: "OrganizationId",
+                principalTable: "Organisations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "OrganizationId",
+                table: "ReferralServices",
+                type: "bigint",
+                nullable: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReferralServices_OrganizationId",
+                table: "ReferralServices",
+                column: "OrganizationId");
+
             migrationBuilder.DropForeignKey(
                 name: "FK_Organisations_ReferralServices_ReferralServiceId",
                 table: "Organisations");
@@ -21,26 +60,6 @@ namespace FamilyHubs.Referral.Data.Migrations
             migrationBuilder.DropColumn(
                 name: "ReferralServiceId",
                 table: "Organisations");
-
-            migrationBuilder.AddColumn<long>(
-                name: "OrganizationId",
-                table: "ReferralServices",
-                type: "bigint",
-                nullable: false,
-                defaultValue: 0L);
-
-            migrationBuilder.CreateIndex(
-                name: "IX_ReferralServices_OrganizationId",
-                table: "ReferralServices",
-                column: "OrganizationId");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_ReferralServices_Organisations_OrganizationId",
-                table: "ReferralServices",
-                column: "OrganizationId",
-                principalTable: "Organisations",
-                principalColumn: "Id",
-                onDelete: ReferentialAction.Cascade);
         }
 
         /// <inheritdoc />
