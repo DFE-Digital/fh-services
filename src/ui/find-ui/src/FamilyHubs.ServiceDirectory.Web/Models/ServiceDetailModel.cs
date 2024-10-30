8392689f0771e85b0f987eb809cff53b786ab67f
Zac-AND-Digital <zac.king@and.digital>
2024-10-30 16:51:21 +0000

    wip: online and/or telephone schedule

--- src/ui/find-ui/src/FamilyHubs.ServiceDirectory.Web/Models/ServiceDetailModel.cs
+++ src/ui/find-ui/src/FamilyHubs.ServiceDirectory.Web/Models/ServiceDetailModel.cs
@@ -9,6 +9,10 @@ public class ServiceDetailModel
     public string? MoreDetails { get; init; }
     public string Deliveries { get; init; } = null!;
 
+    public string? OnlineTelephone { get; init; }
+    public string? DaysAvailable { get; init; }
+    public string? ExtraAvailabilityDetails { get; init; }
+
     public IEnumerable<string> Categories { get; init; } = [];
     public IEnumerable<string> Languages { get; init; } = [];
 
--- src/ui/find-ui/src/FamilyHubs.ServiceDirectory.Web/Pages/ServiceDetail/Index.cshtml.cs
+++ src/ui/find-ui/src/FamilyHubs.ServiceDirectory.Web/Pages/ServiceDetail/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using FamilyHubs.ServiceDirectory.Core.ServiceDirectory.Interfaces;
 using FamilyHubs.ServiceDirectory.Shared.Display;
 using FamilyHubs.ServiceDirectory.Shared.Dto;
@@ -31,15 +32,18 @@ public class Index : PageModel
     private static string GetCostOption(ICollection<CostOptionDto> serviceCostOptions)
         => serviceCostOptions.Count == 0 ? "Free" : "Yes, it costs money to use.";
 
-    private static string GetDeliveries(ICollection<ServiceDeliveryDto> serviceDeliveries)
-        => string.Join(", ", serviceDeliveries.Select(sD => sD.Name).Order());
+    private static IEnumerable<AttendingType> GetDeliveries(ICollection<ServiceDeliveryDto> serviceDeliveries)
+        => serviceDeliveries.Select(x => x.Name).Order();
 
     private static string IsFamilyHub(LocationTypeCategory locationTypeCategory) =>
         locationTypeCategory == LocationTypeCategory.FamilyHub ? "Yes" : "No";
 
+    private static string? GetDaysAvailableOnlineOrTelephone(ScheduleDto? scheduleDto) =>
+        scheduleDto?.ByDay?.Split(',').GetDayNames(); // TODO: FHB-712 - Common Method
+
     private static ScheduleDto? GetScheduleForLocation(long locationId,
         ICollection<ServiceAtLocationDto> serviceAtLocations)
-        => serviceAtLocations.FirstOrDefault(sAl => sAl.LocationId == locationId)?.Schedules.FirstOrDefault();
+        => serviceAtLocations.FirstOrDefault(x => x.LocationId == locationId)?.Schedules.FirstOrDefault();
 
     private static IEnumerable<string> GetAccessibilities(
         ICollection<AccessibilityForDisabilitiesDto> serviceAccessibilityForDisabilities)
@@ -48,14 +52,14 @@ public class Index : PageModel
 
         accessibilities
             .AddRange(serviceAccessibilityForDisabilities
-                .Select(accessibilityForDisabilitiesDto => accessibilityForDisabilitiesDto.Accessibility)
+                .Select(x => x.Accessibility)
                 .OfType<string>());
 
         return accessibilities;
     }
 
     private static string GetDaysAvailable(ScheduleDto locationSchedule) =>
-        (locationSchedule.ByDay?.Split(',')).GetDayNames();
+        (locationSchedule.ByDay?.Split(',')).GetDayNames(); // TODO: FHB-712 - Common Method
 
     private static List<Location> GetLocations(ICollection<LocationDto> serviceLocations,
         ICollection<ServiceAtLocationDto> serviceAtLocations)
@@ -91,21 +95,56 @@ public class Index : PageModel
         };
     }
 
-    private static ServiceDetailModel GetServiceDetailModel(ServiceDto serviceDto) => new()
+    private static ServiceDetailModel GetServiceDetailModel(ServiceDto serviceDto)
     {
-        Name = serviceDto.Name,
-        Summary = serviceDto.Summary,
-        Eligibility = GetEligibility(serviceDto.Eligibilities),
-        Cost = GetCostOption(serviceDto.CostOptions),
-        MoreDetails = serviceDto.Description,
-        Deliveries = GetDeliveries(serviceDto.ServiceDeliveries),
+        ScheduleDto? serviceScheduleDto =
+            serviceDto.Schedules.FirstOrDefault(x => x.ServiceId == serviceDto.Id);
 
-        Categories = serviceDto.Taxonomies.Select(t => t.Name).Order(),
-        Languages = serviceDto.Languages.Select(l => l.Name).Order(),
+        IEnumerable<AttendingType> deliveries = GetDeliveries(serviceDto.ServiceDeliveries);
 
-        Locations = GetLocations(serviceDto.Locations, serviceDto.ServiceAtLocations),
-        Contact = GetContact(serviceDto.Contacts)
-    };
+        StringBuilder deliveryString = new();
+        StringBuilder onlineTelephoneString = new();
+
+        foreach (AttendingType delivery in deliveries) // TODO: FHB-712 - Move into own method, possibly 2 methods!!!
+        {
+            switch (delivery)
+            {
+                case AttendingType.InPerson:
+                    deliveryString.Append("In person");
+                    break;
+                case AttendingType.Online:
+                    deliveryString.Append(", Online");
+                    onlineTelephoneString.Append("Online");
+                    break;
+                case AttendingType.Telephone:
+                    deliveryString.Append(", Telephone");
+                    onlineTelephoneString.Append(onlineTelephoneString.Length == 0 ? "Telephone" : " and telephone");
+                    break;
+            }
+        }
+
+        ServiceDetailModel serviceDetailModel = new()
+        {
+            Name = serviceDto.Name,
+            Summary = serviceDto.Summary,
+            Eligibility = GetEligibility(serviceDto.Eligibilities),
+            Cost = GetCostOption(serviceDto.CostOptions),
+            MoreDetails = serviceDto.Description,
+            Deliveries = deliveryString.ToString(),
+
+            OnlineTelephone = onlineTelephoneString.ToString(),
+            DaysAvailable = GetDaysAvailableOnlineOrTelephone(serviceScheduleDto) ?? "None provided",
+            ExtraAvailabilityDetails = serviceScheduleDto?.Description ?? "None provided",
+
+            Categories = serviceDto.Taxonomies.Select(t => t.Name).Order(),
+            Languages = serviceDto.Languages.Select(l => l.Name).Order(),
+
+            Locations = GetLocations(serviceDto.Locations, serviceDto.ServiceAtLocations),
+            Contact = GetContact(serviceDto.Contacts)
+        };
+
+        return serviceDetailModel;
+    }
 
     public async Task<IActionResult> OnGetAsync(long serviceId)
     {
