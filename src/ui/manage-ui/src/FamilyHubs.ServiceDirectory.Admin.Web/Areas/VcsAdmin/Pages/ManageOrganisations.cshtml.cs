5fd044630c9d41a025a6958126be307a7277e3e3
Zac-AND-Digital <zac.king@and.digital>
2024-11-01 17:03:47 +0000

    refactor: some optimisations

--- src/ui/manage-ui/src/FamilyHubs.ServiceDirectory.Admin.Web/Areas/VcsAdmin/Pages/ManageOrganisations.cshtml.cs
+++ src/ui/manage-ui/src/FamilyHubs.ServiceDirectory.Admin.Web/Areas/VcsAdmin/Pages/ManageOrganisations.cshtml.cs
@@ -13,18 +13,19 @@ namespace FamilyHubs.ServiceDirectory.Admin.Web.Areas.VcsAdmin.Pages
 {
     public class ManageOrganisationsModel : HeaderPageModel
     {
-        private const int _pageSize = 10;
+        private const int PageSize = 10;
+        public const string OrganisationColumn = "Organisation";
+        public const string LaColumn = "LocalAuthority";
+
         private readonly IServiceDirectoryClient _serviceDirectoryClient;
         private readonly ICacheService _cacheService;
 
-        public string OrganisationColumn { get; } = "Organisation";
-        public string LaColumn { get; } = "LocalAuthority";
         public bool IsDfeAdmin { get; set; }
         public IPagination Pagination { get; set; }
         public PaginatedList<OrganisationModel> PaginatedOrganisations { get; set; }
 
         [BindProperty]
-        public int PageNum { get; set; } = 1; //Do not change variable name, this is what is posted by the pagination partial
+        public int PageNum { get; set; } = 1; // Do not change variable name, this is what is posted by the pagination partial
 
         [BindProperty]
         public string SortBy { get; set; } = string.Empty;
@@ -64,16 +65,15 @@ namespace FamilyHubs.ServiceDirectory.Admin.Web.Areas.VcsAdmin.Pages
 
         private async Task SetPaginatedList()
         {
-            var vcsOrganisations = await GetPermittedOrganisations();
-            vcsOrganisations = Sort(vcsOrganisations);
+            ICollection<OrganisationModel> vcsOrganisations = Sort(await GetPermittedOrganisations());
 
             PaginatedOrganisations = new PaginatedList<OrganisationModel>(
-                vcsOrganisations.Skip((PageNum - 1) * _pageSize).Take(_pageSize).ToList(),
-                vcsOrganisations.Count(),
+                vcsOrganisations.Skip((PageNum - 1) * PageSize).Take(PageSize).ToList(),
+                vcsOrganisations.Count,
                 PageNum,
-                _pageSize);
+                PageSize);
 
-            if(PaginatedOrganisations.TotalCount> 0)
+            if (PaginatedOrganisations.TotalCount> 0)
             {
                 Pagination = new LargeSetPagination(PaginatedOrganisations.TotalPages, PageNum);
             }
@@ -83,50 +83,56 @@ namespace FamilyHubs.ServiceDirectory.Admin.Web.Areas.VcsAdmin.Pages
         /// <summary>
         /// Gets the VCS organisations the user is permitted to see
         /// </summary>
-        private async Task<IEnumerable<OrganisationModel>> GetPermittedOrganisations()
+        private async Task<ICollection<OrganisationModel>> GetPermittedOrganisations()
         {
             var user = HttpContext.GetFamilyHubsUser();
-            List<OrganisationDto> organisations;
 
-            if (user.Role == RoleTypes.DfeAdmin)
-            {
-                organisations = await _serviceDirectoryClient.GetOrganisations();
-            }
-            else
-            {
-                organisations = await _serviceDirectoryClient.GetOrganisationByAssociatedOrganisation(long.Parse(user.OrganisationId));
-            }
+            IEnumerable<OrganisationDto> organisations = user.Role == RoleTypes.DfeAdmin
+                ?
+                await _serviceDirectoryClient.GetOrganisations()
+                :
+                await _serviceDirectoryClient.GetOrganisationByAssociatedOrganisation(long.Parse(user.OrganisationId));
 
-            var vcsOrganisations = organisations
+            List<OrganisationModel> vcsOrganisations = organisations
                 .Where(x => x.OrganisationType == Shared.Enums.OrganisationType.VCFS)
                 .Select(org => new OrganisationModel
                 {
                     OrganisationId = org.Id,
                     OrganisationName = org.Name,
-                    LocalAuthority = organisations.Find(x => x.Id == org.AssociatedOrganisationId)?.Name ?? string.Empty
-                });
+                    LocalAuthority = organisations.FirstOrDefault(x => x.Id == org.AssociatedOrganisationId)?.Name ?? string.Empty
+                }).ToList();
 
             return vcsOrganisations;
         }
 
-        private IEnumerable<OrganisationModel> Sort(IEnumerable<OrganisationModel> organisations)
+        private ICollection<OrganisationModel> Sort(ICollection<OrganisationModel> organisations)
         {
             if (string.IsNullOrEmpty(SortBy))
+            {
                 return organisations;
+            }
 
-            if(SortBy == $"{OrganisationColumn}_{SortOrder.Ascending.ToString()}")
-                return organisations.OrderBy(x => x.OrganisationName);
+            if (SortBy == $"{OrganisationColumn}_{SortOrder.Ascending}")
+            {
+                return [..organisations.OrderBy(x => x.OrganisationName)];
+            }
 
-            if (SortBy == $"{OrganisationColumn}_{SortOrder.Descending.ToString()}")
-                return organisations.OrderByDescending(x => x.OrganisationName);
+            if (SortBy == $"{OrganisationColumn}_{SortOrder.Descending}")
+            {
+                return [..organisations.OrderByDescending(x => x.OrganisationName)];
+            }
 
-            if (SortBy == $"{LaColumn}_{SortOrder.Ascending.ToString()}")
-                return organisations.OrderBy(x => x.LocalAuthority);
+            if (SortBy == $"{LaColumn}_{SortOrder.Ascending}")
+            {
+                return [..organisations.OrderBy(x => x.LocalAuthority)];
+            }
 
-            if (SortBy == $"{LaColumn}_{SortOrder.Descending.ToString()}")
-                return organisations.OrderByDescending(x => x.LocalAuthority);
+            if (SortBy == $"{LaColumn}_{SortOrder.Descending}")
+            {
+                return [..organisations.OrderByDescending(x => x.LocalAuthority)];
+            }
 
-            throw new InvalidOperationException("SortBy not recognised");
+            throw new InvalidOperationException($"SortBy cannot be parsed: {SortBy}");
         }
 
         private object CreateQueryParameters()
@@ -140,7 +146,7 @@ namespace FamilyHubs.ServiceDirectory.Admin.Web.Areas.VcsAdmin.Pages
 
         /// <summary>
         /// If someone goes to the edit page then clicks the back button, we want them to return to the
-        /// paginated page they where on. This stores the link to get them back to the current page
+        /// paginated page they were on. This stores the link to get them back to the current page
         /// </summary>
         private async Task CacheParametersToBackButton()
         {
@@ -152,16 +158,16 @@ namespace FamilyHubs.ServiceDirectory.Admin.Web.Areas.VcsAdmin.Pages
                 backButtonPath.Append($"{parameter.Key}={parameter.Value}&");
             }
 
-            backButtonPath = backButtonPath.Remove(backButtonPath.Length - 1, 1);//Remove unwanted '&' or '?'
+            backButtonPath = backButtonPath.Remove(backButtonPath.Length - 1, 1); // Remove unwanted '&' or '?'
 
             await _cacheService.StoreCurrentPageName(backButtonPath.ToString());
         }
 
         public class OrganisationModel
         {
-            public long OrganisationId { get; set; }
-            public string OrganisationName { get; set; } = string.Empty;
-            public string LocalAuthority { get; set; } = string.Empty;
+            public long OrganisationId { get; init; }
+            public string OrganisationName { get; init; } = string.Empty;
+            public string LocalAuthority { get; init; } = string.Empty;
         }
     }
 }
