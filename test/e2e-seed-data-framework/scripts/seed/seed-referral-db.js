d56aaebbe13c4923f1948074b9966cf6d9307542
Zac-AND-Digital <zac.king@and.digital>
2024-11-25 17:01:51 +0000

    fix: directory

--- test/e2e-seed-data-framework/scripts/seed/seed-referral-db.js
+++ test/e2e-seed-data-framework/scripts/seed/seed-referral-db.js
@@ -1,4 +1,4 @@
-import * as Database from "../../../core/referral-db-context.js";
+import * as Database from "../../core/referral-db-context.js";
 
 /**
  * This class contains the implementation of the seeding data for the Referral Db.
