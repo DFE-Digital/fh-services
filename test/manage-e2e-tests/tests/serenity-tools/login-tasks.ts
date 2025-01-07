8a3fe79926ce05bf1a699511d7a67c320c6fd970
Zac-Digital <119302767+Zac-Digital@users.noreply.github.com>
2025-01-07 23:53:09 +0000

    feat: mask values

--- test/manage-e2e-tests/tests/serenity-tools/login-tasks.ts
+++ test/manage-e2e-tests/tests/serenity-tools/login-tasks.ts
@@ -1,10 +1,10 @@
-import { Answerable, Task } from '@serenity-js/core';
+import {Answerable, Masked, Task} from '@serenity-js/core';
 import { Navigate, Click, Enter } from '@serenity-js/web';
 import {startButton, continueButton, emailField, passwordField, signInButton} from "./gov-login-page-objects";
 
 export const loginToTestEnvironment = (): Task =>
     Task.where( `#actor logs into test environment`,
-        Navigate.to(`https://${process.env.USER_NAME}:${process.env.PASSWORD}@signin.integration.account.gov.uk/?prompt=login`)
+        Navigate.to(Masked.valueOf(`https://${process.env.USER_NAME}:${process.env.PASSWORD}@signin.integration.account.gov.uk/?prompt=login`))
     );
 export const navigateToManage= (): Task =>
     Task.where(
@@ -20,8 +20,8 @@ export const loginToManage = (userType: Answerable<string>): Task =>
     Task.where(
         `#actor logs into manage with a gov login email and password`,
         Click.on(signInButton()),
-        Enter.theValue(process.env.DFE_ADMIN_USER).into(emailField()),
+        Enter.theValue(Masked.valueOf(process.env.DFE_ADMIN_USER)).into(emailField()),
         Click.on(continueButton()),
-        Enter.theValue(process.env.GOV_LOGIN_PASSWORD).into(passwordField()),
+        Enter.theValue(Masked.valueOf(process.env.GOV_LOGIN_PASSWORD)).into(passwordField()),
         Click.on(continueButton()),
     );
