8cf5a3aa994e4136634970529337dd58508f2cf4
Tina Gohil <tinagohil@Tinas-MacBook-Pro.local>
2025-01-13 08:52:13 +0000

    test: resolved merge conflict

--- test/manage-e2e-tests/tests/manage-login.spec.ts
+++ test/manage-e2e-tests/tests/manage-login.spec.ts
@@ -5,13 +5,9 @@ import {
     clickOnTheStartButton,
     isTheManageHomepageDisplayed,
     loginToManage,
-<<<<<<<< HEAD:test/manage-e2e-tests/tests/manage-testing.spec.ts
-    loginToTestEnvironment, 
-    isTheManageHomepageDisplayed
-========
     loginToTestEnvironment,
     navigateToManage,
->>>>>>>> 4c17d897747570a75640c867245d6ce74652859d:test/manage-e2e-tests/tests/manage-login.spec.ts
+
 } from './serenity-tools/manage-index';
 
 describe('Manage Tests', () => {
