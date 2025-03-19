32815e683b7f12c75b6fa8dcdd2337ac03acdd34
paul <armstrong.pv@gmail.com>
2025-03-19 12:19:59 +0000

    At the routing rule back in for Find, so the health check will fail and the 502 page will be displayed

--- terraform/modules/fhinfrastructurestack/main.tf
+++ terraform/modules/fhinfrastructurestack/main.tf
@@ -1451,6 +1451,16 @@ resource "azurerm_application_gateway" "sd_ui_app_gateway" {
   }
 
   request_routing_rule {
+    name                       = "${var.prefix}-fh-routing-https-sd-ui"
+    backend_address_pool_name  = "${var.prefix}-${local.appgw_bep_sd_ui_name}"
+    backend_http_settings_name = "${var.prefix}-${local.appgw_backend_sd_ui_name}"
+    http_listener_name         = "${var.prefix}-${local.appgw_listener_https_sd_ui_name}"
+    priority                   = 1
+    rule_type                  = "Basic"
+    rewrite_rule_set_name      = "${var.prefix}-${local.appgw_rewrites_sd_ui_name}"
+  }
+
+  request_routing_rule {
     name                       = "${var.prefix}-fh-routing-http-sd-ui"
     redirect_configuration_name = "${var.prefix}-${local.appgw_redirect_sd_ui_name}"
     http_listener_name         = "${var.prefix}-${local.appgw_listener_http_sd_ui_name}"
